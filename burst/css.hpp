e9ce855142c0d62f29dd92948d9ef775f3e963c0
myun2 <myun2@nwhite.info>
2015-06-04 16:25:29 +0900

    Update tags / css

--- burst/css.hpp
+++ burst/css.hpp
@@ -8,14 +8,19 @@ namespace myun2
 		namespace css
 		{
 			void print_selector(const char* selector) { printf("%s{", selector); }
-			void print_property(const char* attribute, const char* value) { printf("%s:%s;", attribute, value); }
+			void print_property(const char* attribute, const char* value) {
+				printf("%s:%s;", attribute, value); }
 			void print_close() { fputs("}", stdout); }
+
+			void print_font_family(const char* font_families) {
+				print_property("font-family", font_families); }
 
 			void reset()
 			{
 				print_selector("*");
 				print_property("margin", "0");
 				print_property("padding", "0");
+				print_font_family("'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;");
 				print_close();
 			}
 		}
--- burst/html.hpp
+++ burst/html.hpp
@@ -13,19 +13,34 @@ namespace myun2
 			void print_close_tag(const char* tag_name) {
 				printf("</%s>", tag_name); }
 
+			void print_html5_doctype() {
+				printf("<!DOCTYPE html>"); }
+
+			void print_meta_charaset(const char* charset) {
+				printf("<meta charset=\"%s\">", charset); }
+
+			void print_stylesheet_link_tag(const char* path) {
+				printf("<link rel=\"stylesheet\" href=\"%s\">", path); }
+
+			void print_title_tag(const char* title) {
+				print_open_tag ("title");
+				fputs(title, stdout);
+				print_close_tag("title"); }
+
 			template <typename _App>
 			void render(const _App& app)
 			{
-				printf("<!DOCTYPE html>");
+				print_html5_doctype();
 				print_open_tag ("html");
 
+				/*  Head  */
 				print_open_tag ("head");
-				printf("<meta charset=\"UTF-8\">");
-				print_open_tag ("title");
-				fputs(app.title(), stdout);
-				print_close_tag("title");
+				print_meta_charaset("UTF-8");
+				print_stylesheet_link_tag("reset.css");
+				print_title_tag(app.title());
 				print_close_tag("head");
 
+				/*  Body  */
 				print_open_tag ("body");
 				print_open_tag("h1");
 				fputs(app.title(), stdout);
