fedecb22047192030999d2627473721436d24ca4
myun2 <myun2@nwhite.info>
2015-06-04 16:28:03 +0900

    Update

--- burst/css.hpp
+++ burst/css.hpp
@@ -12,14 +12,21 @@ namespace myun2
 				printf("%s:%s;", attribute, value); }
 			void print_close() { fputs("}", stdout); }
 
+			void print_padding(const char* padding) {
+				print_property("padding", padding); }
+
+			void print_margin(const char* margin) {
+				print_property("margin", margin); }
+
 			void print_font_family(const char* font_families) {
 				print_property("font-family", font_families); }
 
 			void reset()
 			{
+				/* Reset */
 				print_selector("*");
-				print_property("margin", "0");
-				print_property("padding", "0");
+				print_padding("0");
+				print_margin("0");
 				print_font_family("'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;");
 				print_close();
 			}
