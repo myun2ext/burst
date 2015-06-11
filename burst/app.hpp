4f5c5e1c71a97897ad06bbc3219ecb35e4309513
myun2 <myun2@nwhite.info>
2015-06-11 17:15:46 +0900

    update app

--- burst/app.hpp
+++ burst/app.hpp
@@ -1,52 +1,35 @@
 #ifndef __MYUN2_GITHUB_COM__BURST__APP_HPP__
 #define __MYUN2_GITHUB_COM__BURST__APP_HPP__
 
+#include "css.hpp"
+#include "html.hpp"
+#include <vector>
+#include <string.h>
+
 namespace myun2
 {
 	namespace burst
 	{
 		struct application_base
 		{
-			virtual const char* bgcolor() const { return "#eee"; }
-			virtual const char* txtcolor() const { return "#000"; }
-			virtual const char* title() const { return "Application"; }
-			virtual void render_html() const =0;
-		};
-
-		struct style_class {
-			virtual const char* name() const =0;
-			virtual const char* content() const { return ""; }
-			virtual const char* inner() const { return ""; }
-			void with(const char* selector) const {
-				printf(".%s %s{", name(), selector);
+			::std::vector<const char*> stylesheets;
+			application_base() {
+				stylesheets.push_back("reset.css");
 			}
-			void close() const { css::print_close(); }
-			void print() const
+			virtual const char* charset() const { return "UTF-8"; }
+			virtual const char* title() const { return ""; }
+
+			void render_body(html_generator_base& r){
+				r.tag("h1", title());
+			}
+
+			void render(const char* path)
 			{
-				printf(".%s{", name());
-				content();
-				css::print_close();
-				inner();
+				if ( strcmp(path, "reset.css") == 0)
+					css::reset(stdout).render();
+				else
+					html_generator(stdout).render(*this);
 			}
-		};
-
-		struct content_base
-		{
-			virtual const char* tag() const { return "div"; };
-			virtual const char* classes() const { return ""; }
-			virtual const char* style() const { return ""; }
-			virtual const char* content() const { return ""; }
-			void print_html() const {
-				printf("<%s class=\"%s\" style=\"%s\">", tag(), classes(), style());
-				fputs(content(), stdout);
-				printf("</%s>", tag());
-			}
-		};
-
-		struct box
-		{
-			virtual const char* margin() const { return "0"; };
-			virtual const char* padding() const { return "0"; }
 		};
 	}
 }
--- example1_simple.cpp
+++ example1_simple.cpp
@@ -1,33 +1,19 @@
-#include "burst/css.hpp"
+#include "burst/app.hpp"
 #include "burst/html.hpp"
-#include <vector>
-#include <string.h>
 using namespace myun2::burst;
 
-struct app_context
+struct example_application : application_base
 {
-	::std::vector<const char*> stylesheets;
-	app_context() {
-		stylesheets.push_back("reset.css");
-	}
-	const char* charset() const { return "UTF-8"; }
-	const char* title() const { return ""; }
-
-	void render_body(html_generator_base& r){
-		r.tag("h1", "Hello World");
-	}
 };
+
 int main(int argc, char *argv[])
 {
-	app_context app;
+	example_application app;
 
 	if (argc > 1)
-	{
-		if ( strcmp(argv[1], "reset.css") == 0)
-			css::reset(stdout).render();
-	}
+		app.render(argv[1]);
 	else
-		html_generator(stdout).render(app);
+		app.render("index.html");
 
 	return 0;
 }
