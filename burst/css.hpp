6436be7b17adce12a14bbf9e606a1b2530d65046
myun2 <myun2@nwhite.info>
2015-06-11 17:21:16 +0900

    flush css.hpp (removed functions)

--- burst/css.hpp
+++ burst/css.hpp
@@ -21,52 +21,6 @@ namespace myun2
 	}
 }
 
-namespace myun2
-{
-	namespace burst
-	{
-		namespace css
-		{
-			void print_selector(const char* selector) { printf("%s{", selector); }
-			void print_property(const char* attribute, const char* value) {
-				printf("%s:%s;", attribute, value); }
-			void print_close() { fputs("}", stdout); }
-
-			void print_background_color(const char* color) {
-				print_property("background-color", color); }
-
-			void print_content(const char* content) {
-				printf("content:\"%s\";", content); }
-
-			void print_text_color(const char* color) {
-				print_property("color", color); }
-
-			void print_text_decoration(const char* style) {
-				print_property("text-decoration", style); }
-
-			void print_font_size(const char* size) {
-				print_property("font-size", size); }
-
-			void print_font_family(const char* font_families) {
-				print_property("font-family", font_families); }
-
-			void print_line_height(const char* height) {
-				print_property("line-height", height); }
-
-			void print_transition(float time_sec) {
-				printf("-webkit-transition:%fs;", time_sec);
-				printf("transition:%fs;", time_sec); }
-
-			template <typename _Fn>
-			void print_style(const char* selector, _Fn fn) {
-				print_selector(selector);
-				fn();
-				print_close();
-			}
-		}
-	}
-}
-
 #include "css/px.hpp"
 #include "css/box.hpp"
 
