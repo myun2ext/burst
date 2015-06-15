d362ef5803bbb7e98f4abbdf88e850ae44b1c4a4
myun2 <myun2@nwhite.info>
2015-06-15 15:57:34 +0900

    html: base

--- burst/html.hpp
+++ burst/html.hpp
@@ -1,73 +1,7 @@
 #ifndef __MYUN2_GITHUB_COM__BURST__HTML_HPP__
 #define __MYUN2_GITHUB_COM__BURST__HTML_HPP__
 
-#include "generator.hpp"
-
-namespace myun2
-{
-	namespace burst
-	{
-		struct html_generator_base : generator
-		{
-			html_generator_base(FILE* f_in) : generator(f_in){}
-
-			void open_tag(const char* tag_name) {
-				generate("<%s>", tag_name); }
-			void start_tag(const char* tag_name) {
-				open_tag(tag_name); }
-
-			void open_tag(const char* tag_name, const char* attributes) {
-				generate("<%s %s>", tag_name, attributes); }
-
-			void close_tag(const char* tag_name) {
-				generate("</%s>", tag_name); }
-			void end_tag(const char* tag_name) {
-				close_tag(tag_name); }
-
-			void tag(const char* tag_name, const char* content) {
-				generate("<%s>%s</%s>", tag_name, content, tag_name); }
-
-			////////////////////////////////////////////
-
-			void div_with_class(const char* class_name) {
-				generate("<div class=\"%s\">", class_name); }
-			void close_div() {
-				generate("</div>"); }
-
-			void start_container() {
-				generate("<div class=\"container\">"); }
-			void close_container() {
-				generate("</div>"); }
-
-			void start_a(const char* link_to) {
-				generate("<a href=\"%s\">", link_to); }
-			void close_a() {
-				generate("</a>"); }
-		};
-
-		namespace html
-		{
-			struct tag : html_generator_base
-			{
-				const char* name;
-				tag(const char* name_in) : name(name_in), html_generator_base(0) {}
-
-				template <typename _Context>
-				void render(_Context& context) {}
-
-				template <typename _Context>
-				void render(FILE* stream, _Context& context)
-				{
-					f = stream;
-					open_tag(name);
-					content(context);
-					close_tag(name);
-				}
-			};
-		}
-	}
-}
-
+#include "html/base.hpp"
 #include "html/head.hpp"
 #include "html/doctype.hpp"
 
