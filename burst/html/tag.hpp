5e0fcdffc621def092384d1a33218149df680e56
myun2 <myun2@nwhite.info>
2015-06-16 18:55:15 +0900

    render content/context (null)

--- burst/html/tag.hpp
+++ burst/html/tag.hpp
@@ -17,24 +17,25 @@ namespace myun2
 
 			struct tag_generator : tag, html_generator_base
 			{
+				struct null_content {
+					template <typename _Context>
+					void render(_Context& context, FILE* stream) {}
+				};
 				tag_generator(const char* name_in, FILE* f_in)
 					: html::tag(name_in), html_generator_base(f_in) {}
 
 				virtual void start() { open_tag(name); }
 				virtual void end() { close_tag(name); }
 
-				template <typename _Context>
-				void render_content(const _Context& context) {}
-
-				template <typename _Context>
-				void render(const _Context& context, FILE* stream = stdout)
+				template <typename _Content, typename _Context>
+				void render(_Content& content, const _Context& context, FILE* stream = stdout)
 				{
 					f = stream;
 					start();
-					render_content(context);
+					content.render(context, stream);
 					end();
 				}
-				void render() { render(0); }
+				void render() { null_content nc; render(nc, 0); }
 			};
 		}
 	}
--- example1_simple.cpp
+++ example1_simple.cpp
@@ -1,19 +1,8 @@
 #include "burst/html.hpp"
-//#include "burst/app.hpp"
 using namespace myun2::burst;
 
-/*
-struct example_application : application_base
-{
-	const char* title() const { return "Example Application"; }
-	void render_content(html_generator_base& r) {
-		r.tag("p", "This is example application.");
-	}
-};
-*/
 int main(int argc, char *argv[])
 {
-	//example_application().render(argv[1]);
 	html_generator<5> h;
 	h.render();
 	return 0;
