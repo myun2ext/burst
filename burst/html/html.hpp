5c8065a4b347bf7c7709abae52b070ce1808d2d2
myun2 <myun2@nwhite.info>
2015-06-16 19:16:31 +0900

    render body

--- burst/html/html.hpp
+++ burst/html/html.hpp
@@ -10,15 +10,20 @@ namespace myun2
 {
 	namespace burst
 	{
+		namespace html
+		{
+			struct body : html::tag_generator {
+				body(FILE* f_in = NULL) : html::tag_generator("body", f_in){}
+			};
+		}
+
 		template <unsigned char _HtmlVersion>
 		struct html_generator : html::tag_generator
 		{
 			html_generator(FILE* f_in = NULL) : html::tag_generator("html", f_in){}
 
-			virtual void render_head(){
-				html::head().render();
-			}
-			virtual void render_body(){}
+			virtual void render_head(){ html::head().render(f); }
+			virtual void render_body(){ html::body().render(f); }
 			virtual void render_content() {
 				render_head();
 				render_body();
