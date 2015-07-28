611eb90bed8c97f08000b2c600dce81b55d23f3e
myun2 <myun2@nwhite.info>
2015-07-28 18:47:08 +0900

    add content

---
+++ burst/content.hpp
@@ -0,0 +1,14 @@
+#ifndef __MYUN2_GITHUB_COM__BURST__CONTENT_HPP__
+#define __MYUN2_GITHUB_COM__BURST__CONTENT_HPP__
+
+namespace myun2
+{
+	namespace burst
+	{
+		struct content_base
+		{
+		};
+	}
+}
+
+#endif//__MYUN2_GITHUB_COM__BURST__CONTENT_HPP__
